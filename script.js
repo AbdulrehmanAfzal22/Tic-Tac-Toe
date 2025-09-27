cbf0bd3dc4120b229a09734c548a42faf6d74198
alikhan <alikhan@aglivo.com>
2025-09-27 21:18:18 +0500

    Patterns updated

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ let para = document.getElementById("para")
 let player1 = true
 
 const patterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [0, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
-]
+  [0, 1, 2], 
+  [3, 4, 5],
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8],
+  [0, 4, 8], 
+  [2, 4, 6] 
+];
 
 box.forEach((boxes) => {
 boxes.addEventListener("click" , ()=> {
