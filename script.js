397441d47f421e8da1191889c0ff356e5d8be788
alikhan <alikhan@aglivo.com>
2025-09-27 21:08:44 +0500

    Tic Tac Toe completed

---
+++ script.js
@@ -0,0 +1,53 @@
+let box = document.querySelectorAll(".btn")
+let reset = document.getElementById("reset")
+let para = document.getElementById("para")
+let player1 = true
+
+const patterns = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [0, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+box.forEach((boxes) => {
+boxes.addEventListener("click" , ()=> {
+   
+    if (player1) {
+        boxes.innerHTML="X"
+        player1= false
+    }
+    else{
+        boxes.innerHTML = "O"
+player1=true
+    }
+    boxes.disabled = true
+    winner()
+})
+})
+const winner = () => {
+  for (let pattern of patterns) {
+    let pos1Val = box[pattern[0]].innerText;
+    let pos2Val = box[pattern[1]].innerText;
+    let pos3Val = box[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+       para.innerHTML = `🎉 Congratulations, Winner is  ${pos1Val}   🏆`
+        return true;
+      }
+    }
+  }
+}
+reset.addEventListener("click", function () {
+  para.innerHTML = "";        
+  player1 = true;               
+  box.forEach(b => {
+    b.innerHTML = "";           
+    b.disabled = false;       
+  });
+});
